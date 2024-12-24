85adfcc5ff3c070dd1d76927324bd38c4b9f980a
Camille Phoebe Legaspi <camillephoebetlegaspi@gmail.com>
2024-12-24 10:54:25 +0800

    Initial commit

---
+++ src/components/SongItem.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { PlayerContext } from '../context/PlayerContext'
+
+const SongItem = (props) => {
+
+  const {playWithId} = useContext(PlayerContext)
+
+  return (
+      <div onClick={()=>playWithId(props.id)}  className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
+      <img className='rounded' src={props.image} alt="" />
+      <p className="font-bold mt-2 mb-1">{props.name}</p>
+      <p className="text-slate-200 text-sm">{props.desc}</p>
+    </div>
+  )
+}
+
+export default SongItem
